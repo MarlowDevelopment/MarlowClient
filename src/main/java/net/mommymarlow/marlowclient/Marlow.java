4aa3e2153be74ec9f22104ce4762d84a2ea64a80
Dinolog <dino.legcevic@gmail.com>
2023-05-23 21:23:29 +0200

    Added icon, fixed autoupdate for feather. Version 0.2

--- src/main/java/net/mommymarlow/marlowclient/Marlow.java
+++ src/main/java/net/mommymarlow/marlowclient/Marlow.java
@@ -32,7 +32,7 @@ public class Marlow implements ModInitializer {
     public EventManager eventManager;
 
     // Version
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
 
     public static final MarlowSystem system = MarlowSystem.INSTANCE;
 
